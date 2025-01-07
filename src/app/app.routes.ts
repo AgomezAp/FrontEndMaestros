da0fdbfb76bbdf4d0f3a51939f9a30c8cd749340
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2025-01-07 09:14:54 -0500

    Refactor routing configuration for improved readability and consistency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,47 +1,44 @@
 import { Routes } from '@angular/router';
 
-import {
-  AgregarMaestroComponent,
-} from './components/agregar-maestro/agregar-maestro.component';
+import { AgregarMaestroComponent } from './components/agregar-maestro/agregar-maestro.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import {
-  HistoricoMaestrosComponent,
-} from './components/historico-maestros/historico-maestros.component';
+import { HistoricoMaestrosComponent } from './components/historico-maestros/historico-maestros.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import {
-  ResetPasswordComponent,
-} from './components/reset-password/reset-password.component';
-import {
-  VerMaestrosActivosComponent,
-} from './components/ver-maestros-activos/ver-maestros-activos.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { VerMaestrosActivosComponent } from './components/ver-maestros-activos/ver-maestros-activos.component';
 
 export const routes: Routes = [
-
-    {
-        path: '', component: LoginComponent
-      },
-      {
-        path: 'logIn', component: LoginComponent
-      },
-      {
-        path: 'signup', component: RegisterComponent
-      },
-      {
-        path: 'reestablecerContraseña', component: ResetPasswordComponent
-      },
-      {
-        path: 'dashBoard', component: DashboardComponent
-      },
-      {
-        path:'agregarMaestro', component: AgregarMaestroComponent
-      },
-      {
-        path: 'ObtenerMaestrosActivos', component: VerMaestrosActivosComponent
-      },
-      {
-        path: 'ver-historico-maestros', component: HistoricoMaestrosComponent
-      }
-
-
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'logIn',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: RegisterComponent,
+  },
+  {
+    path: 'reestablecerContraseña',
+    component: ResetPasswordComponent,
+  },
+  {
+    path: 'dashBoard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'agregarMaestro',
+    component: AgregarMaestroComponent,
+  },
+  {
+    path: 'ObtenerMaestrosActivos',
+    component: VerMaestrosActivosComponent,
+  },
+  {
+    path: 'ver-historico-maestros',
+    component: HistoricoMaestrosComponent,
+  },
 ];
