21f2a0036154d43a585a8b0ed29c54542810352b
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2025-02-10 07:25:56 -0500

    Update environment configuration files for production and development settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
-/* export const environment = {
+export const environment = {
   production: false,
   apiUrl: 'https://api.andrespublicidadmonitoreo.com/',
 };
- */
-export const environment = {
+
+/* export const environment = {
   production: false,
   apiUrl: 'http://localhost:3010/',
-};
+}; */
